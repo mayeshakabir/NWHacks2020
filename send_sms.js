66cfe2a4dd8c5791ecec63eb6df292cf36b28715
Albert Yip <albertkaiyip@gmail.com>
2020-01-11 15:14:47 -0800

    Twilio receive and reply skeleton

---
+++ send_sms.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+12563635318',
+     to: '+16046002350'
+   })
+  .then(message => console.log(message.sid));
