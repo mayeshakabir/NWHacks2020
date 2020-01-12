6653a72e1e1a4dfa58ccc804f870661292a82812
Mayesha Kabir <mayeshakabir@gmail.com>
2020-01-12 11:31:17 -0800

    removed foreach comment

--- server.js
+++ server.js
@@ -47,13 +47,7 @@ app.post('/sms', (req, res) => {
     const twiml = new MessagingResponse();
 
     let instStr = resp.join("\n");
-
-    /*
-    resp.forEach(inst => {
-        twiml.message(inst);
-    });
-    */
-   twiml.message(instStr);
+    twiml.message(instStr);
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());   
